7d5dd9761bfaf1eeb7c4c1184e6d5e268c53580b
Gary Lai <garylai1990@gmail.com>
2020-06-27 16:45:03 +0800

    :zap: (seo) imporove performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,13 +13,18 @@ function App({ Component, pageProps }: AppProps) {
         title={`Roy Lin 林昆彥`}
         description={`林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.`}
         openGraph={{
+          title: "Roy Lin 林昆彥",
+          description:
+            "林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.",
           type: "website",
           locale: "en",
           url: "https://rokykung.com",
           site_name: "Roy Lin 林昆彥",
           images: [
             {
-              url: "https://roykung.com/profile.png",
+              url: "https://roykung.com/profile.jpeg",
+              width: 592,
+              height: 569,
               alt: "Roy Lin 林昆彥",
             },
           ],
