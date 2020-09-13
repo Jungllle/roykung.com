d9d23613eebb011308a42e2a9bcfcf7fbfe6238c
Gary Lai <garylai1990@gmail.com>
2020-09-13 22:33:27 +0800

    :recycle: (refactor)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,42 +6,40 @@ import { Container } from "react-bootstrap";
 import Copyright from "@components/Copyright";
 import { DefaultSeo } from "next-seo/lib";
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <DefaultSeo
-        title={`Roy Lin 林昆彥`}
-        description={`林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.`}
-        openGraph={{
-          title: "Roy Lin 林昆彥",
-          description:
-            "林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.",
-          type: "website",
-          locale: "en",
-          url: "https://rokykung.com",
-          site_name: "Roy Lin 林昆彥",
-          images: [
-            {
-              url: "https://roykung.com/profile.jpeg",
-              width: 592,
-              height: 569,
-              alt: "Roy Lin 林昆彥",
-            },
-          ],
-        }}
-        twitter={{
-          handle: "@roylinkung",
-          site: "@roylinkung",
-          cardType: "summary_large_image",
-        }}
-      />
-      <Nav />
-      <Container>
-        <Component {...pageProps} />
-        <Copyright />
-      </Container>
-    </div>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <DefaultSeo
+      title={`Roy Lin 林昆彥`}
+      description={`林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.`}
+      openGraph={{
+        title: "Roy Lin 林昆彥",
+        description:
+          "林昆彥 (Roy Lin). I'm a dreamer, singer and basketball player, loving jogging, reading and cooking, and now running a startup in Taipei, Taiwan.",
+        type: "website",
+        locale: "en",
+        url: "https://rokykung.com",
+        site_name: "Roy Lin 林昆彥",
+        images: [
+          {
+            url: "https://roykung.com/profile.jpeg",
+            width: 592,
+            height: 569,
+            alt: "Roy Lin 林昆彥",
+          },
+        ],
+      }}
+      twitter={{
+        handle: "@roylinkung",
+        site: "@roylinkung",
+        cardType: "summary_large_image",
+      }}
+    />
+    <Nav/>
+    <Container>
+      <Component {...pageProps} />
+      <Copyright/>
+    </Container>
+  </div>
+);
 
 export default App;
