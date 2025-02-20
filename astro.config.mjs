06f236dff2908f69456c154a08e6ae2b82e64bb6
Yi-Ru Lin <deduce@gmail.com>
2025-02-19 18:59:01 -0600

    chore: update project configuration for Astro and Tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react(),
+  ],
+  site: 'https://roykung.com',
+}); 
