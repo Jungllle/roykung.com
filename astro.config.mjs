b60e2e85d51f3b04c1b1f52c6b0095c7c8f7aa65
Yi-Ru Lin <deduce@gmail.com>
2025-02-19 19:01:56 -0600

    feat: add Cloudflare adapter support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [
     tailwind(),
     react(),
   ],
+  output: 'server',
+  adapter: cloudflare({
+    mode: 'directory'
+  }),
   site: 'https://roykung.com',
 }); 
